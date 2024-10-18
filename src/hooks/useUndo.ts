d0f959124d841abe9475f47257a7000dfb58a4b1
sunyfg <yanfeng_sun@yeah.net>
2024-10-18 15:48:48 +0800

    feat: 使用 useReducer 优化 useUndo

--- src/hooks/useUndo.ts
+++ src/hooks/useUndo.ts
@@ -1,29 +1,27 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer } from "react";
 
-/**
- * 自定义撤销/重做钩子
- * @param initialPresent initial state
- */
-export const useUndo = <T>(initialPresent: T) => {
-  const [state, setState] = useState<{
-    past: T[]; // past 用于存储已经撤销的状态
-    present: T; // present 用于存储当前状态
-    future: T[]; // future 用于存储已经重做的状态
-  }>({
-    past: [],
-    present: initialPresent,
-    future: [],
-  });
+const UNDO = "UNDO";
+const REDO = "REDO";
+const SET = "SET";
+const RESET = "RESET";
 
-  // canUndo 和 canRedo 用于判断是否可以撤销和重做
-  const canUndo = state.past.length > 0;
-  const canRedo = state.future.length > 0;
+type State<T> = {
+  past: T[]; // past 用于存储已经撤销的状态
+  present: T; // present 用于存储当前状态
+  future: T[]; // future 用于存储已经重做的状态
+};
 
-  // undo 函数用于撤销操作
-  const undo = useCallback(() => {
-    setState((prevState) => {
-      const { past, present, future } = prevState;
-      if (past.length === 0) return prevState;
+type Action<T> = {
+  type: typeof UNDO | typeof REDO | typeof SET | typeof RESET;
+  newPresent?: T;
+};
+
+const undoReducer = <T>(state: State<T>, actions: Action<T>) => {
+  const { past, present, future } = state;
+  const { type, newPresent } = actions;
+  switch (type) {
+    case UNDO: {
+      if (past.length === 0) return state;
 
       const previous = past[past.length - 1]; // 获取最后一个 past 状态
       const newPast = past.slice(0, past.length - 1); // 删除最后一个 past 状态
@@ -33,14 +31,9 @@ export const useUndo = <T>(initialPresent: T) => {
         present: previous,
         future: [present, ...future],
       };
-    });
-  }, []);
-
-  // redo 函数用于重做操作
-  const redo = useCallback(() => {
-    setState((prevState) => {
-      const { past, present, future } = prevState;
-      if (future.length === 0) return prevState;
+    }
+    case REDO: {
+      if (future.length === 0) return state;
 
       const next = future[0]; // 获取第一个 future 状态
       const newFuture = future.slice(1); // 删除第一个 future 状态
@@ -50,30 +43,61 @@ export const useUndo = <T>(initialPresent: T) => {
         present: next,
         future: newFuture,
       };
-    });
-  }, []);
+    }
 
-  // set 函数用于设置当前状态
-  const set = useCallback((newPresent: T) => {
-    setState((prevState) => {
-      const { past, present, future } = prevState;
-      if (newPresent === present) return prevState;
+    case SET: {
+      if (newPresent === present) return state;
 
       return {
         past: [...past, present],
         present: newPresent,
         future: [],
       };
-    });
-  }, []);
+    }
 
-  const reset = useCallback((newPresent: T) => {
-    setState({
-      past: [],
-      present: newPresent,
-      future: [],
-    });
-  }, []);
+    case RESET: {
+      return {
+        past: [],
+        present: newPresent,
+        future: [],
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+/**
+ * 自定义撤销/重做钩子
+ * @param initialPresent initial state
+ */
+export const useUndo = <T>(initialPresent: T) => {
+  const [state, dispatch] = useReducer(undoReducer, {
+    past: [],
+    present: initialPresent,
+    future: [],
+  } as State<T>);
+
+  // canUndo 和 canRedo 用于判断是否可以撤销和重做
+  const canUndo = state.past.length > 0;
+  const canRedo = state.future.length > 0;
+
+  // undo 函数用于撤销操作
+  const undo = useCallback(() => dispatch({ type: UNDO }), []);
+
+  // redo 函数用于重做操作
+  const redo = useCallback(() => dispatch({ type: REDO }), []);
+
+  // set 函数用于设置当前状态
+  const set = useCallback(
+    (newPresent: T) => dispatch({ type: SET, newPresent }),
+    [],
+  );
+
+  const reset = useCallback(
+    (newPresent: T) => dispatch({ type: RESET, newPresent }),
+    [],
+  );
 
   return [
     state,
